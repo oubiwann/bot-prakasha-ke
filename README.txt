a53804d75919fd7f3813424bafc8c24ef55ba290
Christopher Armstrong <radix@twistedmatrix.com>
2007-02-14 19:47:01 -0500

    hooray! use bzr branch config data instead of hacking .py files

--- README.txt
+++ README.txt
@@ -13,10 +13,16 @@ Bazaar commit hook
 There's a bzr commit hook in bzrcommitmessage.py. Edit it to specify
 configuration details about your listener and the channel to send to.
 
-Copy it to ~/.bazaar/plugins, then edit ~/.bazaar/locations.conf, creating or
-editing an entry for whatever branch or branch hierarchy you want the hook to
-apply to, like so:
+symlink it into ~/.bazaar/plugins, then edit ~/.bazaar/locations.conf,
+creating or editing an entry for whatever branch or branch hierarchy
+you want the hook to apply to, like so:
 
 [/home/radix/Projects/myproj]
 post_commit = bzrlib.plugins.bzrcommitmessage.post_commit
+message_host = 'commitbot.example.com'
+message_port = 1234
+message_password = 'yay'
+message_channel = '#bottest'
 
+Now any commits that happen under /home/radix/Projects/myproj will
+send a message to #bottest.
--- bzrcommitmessage.py
+++ bzrcommitmessage.py
@@ -1,18 +1,19 @@
-HOST = 'commitbot.example.com'
-PORT = 1234
-PASSWORD = 'password'
-CHANNEL = '#channel'
-
 def send_commit(branch, rev_id):
+    config = branch.get_config()
+    host = config.get_user_option('message_host').encode('ascii')
+    port = int(config.get_user_option('message_port'))
+    password = config.get_user_option('message_password').encode('utf-8')
+    channel = config.get_user_option('message_channel').encode('utf-8')
+
     revision = branch.repository.get_revision(rev_id)
     message = "%s [%s] - %s" % (revision.committer,
                                 branch.get_config().get_nickname(),
                                 revision.message)
     import socket
     s = socket.socket()
-    s.connect((HOST, PORT))
+    s.connect((host, port))
     packit = ''
     for line in message.splitlines():
-        packit += '%s:%s:%s\r\n' % (PASSWORD, CHANNEL, line)
+        packit += '%s:%s:%s\r\n' % (password, channel, line)
     s.sendall(packit)
     s.close()
