c6aad2f269d39a10030da53a2ce3c435a4f7e725
Jamu Kakar <jkakar@kakar.ca>
2007-02-15 16:02:51 -0800

    - New message shows e-mail address, branch nick and revision number,   with commit message following.

--- bzrcommitmessage.py
+++ bzrcommitmessage.py
@@ -6,9 +6,9 @@ def send_commit(branch, rev_id):
     channel = config.get_user_option('message_channel').encode('utf-8')
 
     revision = branch.repository.get_revision(rev_id)
-    message = "%s [%s] - %s" % (revision.committer,
-                                branch.get_config().get_nickname(),
-                                revision.message)
+    message = "%s - %s r%d\r\n%s" % (branch.get_config().user_email(),
+                                     branch.get_config().get_nickname(),
+                                     branch.revno(), revision.message)
     import socket
     s = socket.socket()
     s.connect((host, port))
