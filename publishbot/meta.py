e5cbe0d37c0f4f96f95bc3c3c46f3498a9b78dd6
Duncan McGreggor <duncan@dreamhost.com>
2012-04-21 23:46:00 -0400

    Added a meta file.

---
+++ publishbot/meta.py
@@ -0,0 +1,10 @@
+display_name = "bot-prakasha-ke"
+library_name = "prakasha"
+version = "0.3"
+author = "Duncan McGreggor"
+author_email = "duncan@dreamhost.com"
+license = "BSD"
+url = "http://launchpad.net/%s" % display_name
+description = "A log-publishing bot featuring a shell you can ssh into and run commands from"
+
+
