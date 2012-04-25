6dd57e8fa2a22ab3c17aafb83412a21a16692ef9
Duncan McGreggor <duncan@dreamhost.com>
2012-04-25 02:19:20 -0400

    * Added an SFTP implementation that aims to push files into the running memory   of the bot. * More TODO updates.

--- prakasha/shell.py
+++ prakasha/shell.py
@@ -4,12 +4,16 @@ import os
 from pprint import pprint
 import sys
 
+from zope import interface
+
 from twisted.cred import checkers, portal
-from twisted.conch import manhole, manhole_ssh
+from twisted.conch import interfaces, manhole, manhole_ssh, unix
 from twisted.conch.checkers import SSHPublicKeyDatabase
+from twisted.conch.ssh import filetransfer, session
+from twisted.conch.ssh.channel import SSHChannel
 from twisted.conch.ssh.factory import SSHFactory
 from twisted.conch.ssh.keys import Key
-from twisted.python import failure, log
+from twisted.python import components, failure, log
 
 from zope.interface import implements
 
@@ -48,27 +52,137 @@ class MOTDColoredManhole(manhole.ColoredManhole):
         return config.ssh.banner or "Welcome to MOTDColoredManhole!"
 
 
-class ExecutingTerminalSession(manhole_ssh.TerminalSession):
+
+class TerminalSession(manhole_ssh.TerminalSession):
     """
     """
     def windowChanged(self, coords):
         log.msg("New coordinates: %s" % str(coords))
 
-    def execCommand(self, proto, cmd):
-        avatar = proto.session.avatar
-        namespace = updateNamespace(proto.session.session.namespace)
+
+class ExecutingTerminalSession(TerminalSession):
+    """
+    """
+    def _processShellCommand(self, cmd, namespace):
         try:
             result = eval(cmd, namespace)
         except NameError:
             command = cmd.split("(")[0]
             msg = "Command '%s' not found in namespace!" % command
             raise exceptions.IllegalAPICommand(msg)
-        avatar.conn.transport.loseConnection()
+
+    def execCommand(self, proto, cmd):
+        avatar = proto.session.avatar
+        conn = avatar.conn
+        namespace = updateNamespace(proto.session.session.namespace)
+        if cmd.startswith("scp"):
+            # XXX raise custom error
+            pass
+        else:
+            self._processShellCommand(cmd, namespace)
+            avatar.conn.transport.loseConnection()
+
+
+class SFTPEnabledTerminalUser(manhole_ssh.TerminalUser, unix.UnixConchUser):
+    """
+    """
+    def __init__(self, original, avatarId):
+        manhole_ssh.TerminalUser.__init__(self, original, avatarId)
+        unix.UnixConchUser.__init__(self, avatarId)
+
+    def _runAsUser(self, f, *args, **kw):
+        euid = os.geteuid()
+        egid = os.getegid()
+        groups = os.getgroups()[:16]
+        uid, gid = self.getUserGroupId()
+        os.setegid(0)
+        os.seteuid(0)
+        os.setgroups(self.getOtherGroups())
+        os.setegid(gid)
+        os.seteuid(uid)
+        try:
+            f = iter(f)
+        except TypeError:
+            f = [(f, args, kw)]
+        try:
+            for i in f:
+                func = i[0]
+                args = len(i)>1 and i[1] or ()
+                kw = len(i)>2 and i[2] or {}
+                r = func(*args, **kw)
+        finally:
+            os.setegid(0)
+            os.seteuid(0)
+            os.setgroups(groups)
+            os.setegid(egid)
+            os.seteuid(euid)
+        return r
+
+
+class SessionForTerminalUser(object):
+    """
+    """
+
+    def __init__(self, avatar):
+        self.avatar = avatar
+
+
+class MemorySFTPFile(object):
+    """
+    """
+    interface.implements(interfaces.ISFTPFile)
+
+    def __init__(self, server, filename, flags, attrs):
+        self.server = server
+        self.filename = filename
+
+    def close(self):
+        pass
+
+    def readChunk(self, offset, length):
+        pass
+
+    def writeChunk(self, offset, data):
+        pass
+
+
+class MemorySFTPDirectory(object):
+    """
+    """
+    def __init__(self, server, directory):
+        self.server = server
+        self.files = ['a', 'b']
+        self.dir = directory
+
+    def __iter__(self):
+        return self
+
+    def next(self):
+        try:
+           f = self.files.pop(0)
+        except IndexError:
+            raise StopIteration
+        else:
+            return (f, "c", {})
+
+    def close(self):
+        self.files = []
+
+
+class ModifiedSFTPServer(unix.SFTPServerForUnixConchUser):
+    """
+    """
+    def openFile(self, filename, flags, attrs):
+        return MemorySFTPFile(self, self._absPath(filename), flags, attrs)
+
+    def openDirectory(self, path):
+        return MemorySFTPDirectory(self, self._absPath(path))
 
 
 class ExecutingTerminalRealm(manhole_ssh.TerminalRealm):
     """
     """
+    userFactory = SFTPEnabledTerminalUser
     sessionFactory = ExecutingTerminalSession
 
     def __init__(self, namespace):
@@ -221,3 +335,12 @@ def getShellFactory(**namespace):
     factory.publicKeys = {'ssh-rsa': getPubKey()}
     factory.portal.registerChecker(SSHPublicKeyDatabase())
     return factory
+
+
+components.registerAdapter(
+    ModifiedSFTPServer, SFTPEnabledTerminalUser,
+    filetransfer.ISFTPServer)
+
+components.registerAdapter(
+    SessionForTerminalUser, SFTPEnabledTerminalUser,
+    session.ISession)
