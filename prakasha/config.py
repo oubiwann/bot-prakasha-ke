d4ba0550d43ad800d0119291294601f6ad565344
Duncan McGreggor <duncan@dreamhost.com>
2012-05-19 11:51:43 -0400

    Updated to use the new Configurator class.

--- prakasha/config.py
+++ prakasha/config.py
@@ -4,43 +4,21 @@ import os
 
 from zope.interface import moduleProvides
 
+from dreamssh.config import Config, Configurator, main, ssh
 from dreamssh.sdk import interfaces
 
 
 moduleProvides(interfaces.IConfig)
 
 
-class Config(object):
-    pass
-
-
 # Main
-main = Config()
-main.config = Config()
 main.config.userdir = os.path.expanduser("~/.prakasha-bot")
-main.config.localfile = "config.ini"
 main.config.userfile = "%s/%s" % (main.config.userdir, main.config.localfile)
 
-# IRC
-irc = Config()
-irc.servicename = "IRC Client"
-irc.nick = 'prakasha-bot'
-irc.server = 'irc.freenode.net'
-irc.port = 6667
-irc.serverPassword = None
-irc.sslEnabled = False
-irc.lineRate = 1
-
 # Internal SSH Server
-ssh = Config()
 ssh.servicename = "SSH Server"
 ssh.port = 6622
-ssh.pidfile = "twistd.pid"
-ssh.username = "root"
 ssh.keydir = ".prakasha-ssh"
-ssh.privkey = "id_rsa"
-ssh.pubkey = "id_rsa.pub"
-ssh.localdir = "~/.ssh"
 ssh.banner = """:
 : Welcome to
 :
@@ -72,6 +50,16 @@ ssh.banner = """:
 : Enjoy!
 :
 """
+
+# IRC
+irc = Config()
+irc.servicename = "IRC Client"
+irc.nick = 'prakasha-bot'
+irc.server = 'irc.freenode.net'
+irc.port = 6667
+irc.serverPassword = None
+irc.sslEnabled = False
+irc.lineRate = 1
 
 # Listener
 listener = Config()
@@ -110,140 +98,111 @@ log.http.users = {
     }
 log.http.userdb = "httpd.password"
 
-def buildDefaults():
-    config = SafeConfigParser()
-    config.add_section("IRC")
-    config.set("IRC", "servicename", irc.servicename)
-    config.set("IRC", "nick", irc.nick)
-    config.set("IRC", "server", irc.server)
-    config.set("IRC", "port", str(irc.port))
-    config.set("IRC", "serverPassword", irc.serverPassword or "")
-    config.set("IRC", "sslEnabled", str(irc.sslEnabled))
-    config.set("IRC", "lineRate", str(irc.lineRate))
 
-    config.add_section("SSH")
-    config.set("SSH", "servicename", ssh.servicename)
-    config.set("SSH", "port", str(ssh.port))
-    config.set("SSH", "pidfile", ssh.pidfile)
-    config.set("SSH", "username", ssh.username)
-    config.set("SSH", "keydir", ssh.keydir)
-    config.set("SSH", "privkey", ssh.privkey)
-    config.set("SSH", "pubkey", ssh.pubkey)
-    config.set("SSH", "localdir", ssh.localdir)
-    config.set("SSH", "banner", ssh.banner)
+class PrakashaConfigurator(Configurator):
 
-    config.add_section("Listener")
-    config.set("Listener", "servicename", listener.servicename)
-    config.set("Listener", "host", listener.host)
-    config.set("Listener", "port", str(listener.port))
-    config.set("Listener", "password", listener.password)
+    def __init__(self, main, ssh, irc, listener, log):
+        super(PrakashaConfigurator, self).__init__(main, ssh)
+        self.irc = irc
+        self.listener = listener
+        self.log = log
 
-    config.add_section("Logger")
-    config.set("Logger", "servicename", log.servicename)
-    config.set("Logger", "nick", log.nick)
-    config.set("Logger", "channels", json.dumps(log.channels))
+    def buildDefaults(self):
+        config = super(PrakashaConfigurator, self).buildDefaults()
+        config.add_section("IRC")
+        config.set("IRC", "servicename", self.irc.servicename)
+        config.set("IRC", "nick", self.irc.nick)
+        config.set("IRC", "server", self.irc.server)
+        config.set("IRC", "port", str(self.irc.port))
+        config.set("IRC", "serverPassword", self.irc.serverPassword or "")
+        config.set("IRC", "sslEnabled", str(self.irc.sslEnabled))
+        config.set("IRC", "lineRate", str(self.irc.lineRate))
 
-    config.add_section("Rotator")
-    config.set("Rotator", "servicename", log.rotate.servicename)
-    config.set("Rotator", "checkInterval", str(log.rotate.checkInterval))
-    config.set("Rotator", "time", log.rotate.time)
-    config.set("Rotator", "maxAgeHours", str(log.rotate.maxAgeHours))
+        config.add_section("Listener")
+        config.set("Listener", "servicename", self.listener.servicename)
+        config.set("Listener", "host", self.listener.host)
+        config.set("Listener", "port", str(self.listener.port))
+        config.set("Listener", "password", self.listener.password)
 
-    config.add_section("HTTP")
-    config.set("HTTP", "servicename", log.http.servicename)
-    config.set("HTTP", "port", str(log.http.port))
-    config.set("HTTP", "docRoot", log.http.docRoot)
-    config.set("HTTP", "vhostEnabled", str(log.http.vhostEnabled))
-    config.set("HTTP", "vhost", log.http.vhost)
-    config.set("HTTP", "auth", str(log.http.auth))
-    config.set("HTTP", "realm", log.http.realm)
-    config.set("HTTP", "users", json.dumps(log.http.users))
-    config.set("HTTP", "userdb", log.http.userdb)
+        config.add_section("Logger")
+        config.set("Logger", "servicename", self.log.servicename)
+        config.set("Logger", "nick", self.log.nick)
+        config.set("Logger", "channels", json.dumps(self.log.channels))
 
-    return config
+        config.add_section("Rotator")
+        config.set("Rotator", "servicename", self.log.rotate.servicename)
+        config.set(
+            "Rotator", "checkInterval", str(self.log.rotate.checkInterval))
+        config.set("Rotator", "time", self.log.rotate.time)
+        config.set("Rotator", "maxAgeHours", str(self.log.rotate.maxAgeHours))
 
+        config.add_section("HTTP")
+        config.set("HTTP", "servicename", self.log.http.servicename)
+        config.set("HTTP", "port", str(self.log.http.port))
+        config.set("HTTP", "docRoot", self.log.http.docRoot)
+        config.set("HTTP", "vhostEnabled", str(self.log.http.vhostEnabled))
+        config.set("HTTP", "vhost", self.log.http.vhost)
+        config.set("HTTP", "auth", str(self.log.http.auth))
+        config.set("HTTP", "realm", self.log.http.realm)
+        config.set("HTTP", "users", json.dumps(self.log.http.users))
+        config.set("HTTP", "userdb", self.log.http.userdb)
+        return config
 
-def getConfigFile():
-    if os.path.exists(main.config.localfile):
-        return main.config.localfile
+    def updateConfig(self):
+        config = super(PrakashaConfigurator, self).updateConfig()
+        irc = self.irc
+        listener = self.listener
+        log = self.log
+        http = self.log.http
+        rotate = self.log.rotate
 
-    if not os.path.exists(main.config.userdir):
-        os.mkdir(os.path.expanduser(main.config.userdir))
-    return main.config.userfile
+        # IRC
+        irc.servicename = config.get("IRC", "servicename")
+        irc.nick = str(config.get("IRC", "nick"))
+        irc.server = str(config.get("IRC", "server"))
+        irc.port = int(config.get("IRC", "port"))
+        irc.serverPassword = config.get("IRC", "serverPassword") or None
+        sslEnabled = config.get("IRC", "sslEnabled")
+        if sslEnabled.lower() == "true":
+            sslEnabled = True
+        else:
+            sslEnabled = False
+        irc.sslEnabled = sslEnabled
+        irc.lineRate = int(config.get("IRC", "lineRate"))
 
+        # Listener
+        listener.servicename = config.get("Listener", "servicename")
+        listener.host = config.get("Listener", "host")
+        listener.port = int(config.get("Listener", "port"))
+        listener.password = config.get("Listener", "password")
 
-def writeDefaults():
-    config = buildDefaults()
-    with open(getConfigFile(), "wb") as configFile:
-        config.write(configFile)
+        # Log
+        log.servicename = config.get("Logger", "servicename")
+        log.nick = str(config.get("Logger", "nick"))
+        log.channels = [str(x) for x in
+                        json.loads(config.get("Logger", "channels"))]
 
+        # Log rotator
+        rotate.servicename = config.get("Rotator", "servicename")
+        rotate.checkInterval = int(config.get("Rotator", "checkInterval"))
+        rotate.time = config.get("Rotator", "time") 
+        rotate.maxAgeHours = int(config.get("Rotator", "maxAgeHours"))
 
-def updateConfig():
-    config = SafeConfigParser()
-    config.read(getConfigFile())
+        # Log web server
+        http.servicename = config.get("HTTP", "servicename")
+        http.port = int(config.get("HTTP", "port"))
+        http.docRoot = config.get("HTTP", "docRoot")
+        http.vhostEnabled = bool(config.get("HTTP", "vhostEnabled"))
+        http.vhost= config.get("HTTP", "vhost")
+        http.auth = config.get("HTTP", "auth")
+        http.realm = config.get("HTTP", "realm")
 
-    # IRC
-    irc.servicename = config.get("IRC", "servicename")
-    irc.nick = str(config.get("IRC", "nick"))
-    irc.server = str(config.get("IRC", "server"))
-    irc.port = int(config.get("IRC", "port"))
-    irc.serverPassword = config.get("IRC", "serverPassword") or None
-    sslEnabled = config.get("IRC", "sslEnabled")
-    if sslEnabled == "True":
-        sslEnabled = True
-    else:
-        sslEnabled = False
-    irc.sslEnabled = sslEnabled
-    irc.lineRate = int(config.get("IRC", "lineRate"))
-
-    # Internal SSH Server
-    ssh.servicename = config.get("SSH", "servicename")
-    ssh.port = int(config.get("SSH", "port"))
-    ssh.pidfile = str(config.get("SSH", "pidfile"))
-    ssh.username = str(config.get("SSH", "username"))
-    ssh.keydir = config.get("SSH", "keydir")
-    ssh.privkey = config.get("SSH", "privkey")
-    ssh.pubkey = config.get("SSH", "pubkey")
-    ssh.localdir = config.get("SSH", "localdir")
-    ssh.banner = str(config.get("SSH", "banner"))
-
-    # Listener
-    listener.servicename = config.get("Listener", "servicename")
-    listener.host = config.get("Listener", "host")
-    listener.port = int(config.get("Listener", "port"))
-    listener.password = config.get("Listener", "password")
-
-    # Log
-    log.servicename = config.get("Logger", "servicename")
-    log.nick = str(config.get("Logger", "nick"))
-    log.channels = [str(x) for x in
-                    json.loads(config.get("Logger", "channels"))]
-
-    # Log rotator
-    log.rotate.servicename = config.get("Rotator", "servicename")
-    log.rotate.checkInterval = int(config.get("Rotator", "checkInterval"))
-    log.rotate.time = config.get("Rotator", "time") 
-    log.rotate.maxAgeHours = int(config.get("Rotator", "maxAgeHours"))
-
-    # Log web server
-    log.http.servicename = config.get("HTTP", "servicename")
-    log.http.port = int(config.get("HTTP", "port"))
-    log.http.docRoot = config.get("HTTP", "docRoot")
-    log.http.vhostEnabled = bool(config.get("HTTP", "vhostEnabled"))
-    log.http.vhost= config.get("HTTP", "vhost")
-    log.http.auth = config.get("HTTP", "auth")
-    log.http.realm = config.get("HTTP", "realm")
-
-    # Log web server users
-    log.http.users = json.loads(config.get("HTTP", "users"))
-    log.http.userdb = config.get("HTTP", "userdb")
+        # Log web server users
+        http.users = json.loads(config.get("HTTP", "users"))
+        http.userdb = config.get("HTTP", "userdb")
 
 
-configFile = getConfigFile()
-if not os.path.exists(configFile):
-    writeDefaults()
-updateConfig()
+PrakashaConfigurator(main, ssh, irc, listener, log)
 
 
-del Config
-del configFile, updateConfig
+del PrakashaConfigurator
