0ba36a00e3b72987a32daca043ab0980d21c51cf
Duncan McGreggor <duncan@dreamhost.com>
2012-05-28 19:04:43 -0400

    Moved API code into its own module.

---
+++ prakasha/app/api.py
@@ -0,0 +1,109 @@
+from pprint import pprint
+
+from dreamssh.app.shell import base as pythonshell
+from dreamssh.sdk import registry
+
+
+config = registry.getConfig()
+
+
+class CommandAPI(pythonshell.CommandAPI):
+
+    def __init__(self, loggerFactory, publisher):
+        super(CommandAPI, self).__init__()
+        self.loggerFactory = loggerFactory
+        self.publisher = publisher
+        self.namespace = None
+
+    def getLoggedChannels(self):
+        """
+        Get the channels that are being logged.
+        """
+        return self.loggerFactory.channels
+
+    def getJoinedChannels(self):
+        """
+        Get the channels to which this client has actually joined.
+        """
+        return self.publisher.userData.keys()
+
+    def getUsers(self, channel):
+        """
+        For a given channel, get the users that have joined it.
+        """
+        return self.publisher.getUsers(channel)
+
+    def ls(self):
+        """
+        List the objects in the current namespace, in alphabetical order.
+        """
+        keys = sorted(self.namespace.keys())
+        pprint(keys)
+
+    def banner(self):
+        """
+        Display the login banner and associated help or info.
+        """
+        print config.ssh.banner
+
+    def joinAll(self):
+        """
+        Join all the channels to which this client is configured to have
+        access.
+        """
+        for channel in self.getLoggedChannels():
+            self.publisher.join(channel)
+
+    def say(self, channel, message):
+        """
+        A convenience wrapper for the IRC client method of the same name.
+        """
+        self.publisher.send(channel, message)
+
+    def sayMulti(self, data, broadcastMessage=""):
+        """
+        Send a public message to multipl channels. The 'data' parameter is a
+        dict with the keys being the channel names and the values being the
+        message to say on that channel. If a 'broadcastMessage' is supplied,
+        the values in the dict are ignored, and all channels will used the
+        broadcast message.
+        """
+        for channel, message in data.items():
+            if broadcastMessage:
+                message = broadcastMessage
+            self.say(channel, message)
+
+    def sayAll(self, message):
+        """
+        Send a public message to all logged channels.
+        """
+        for channel in self.getLoggedChannels():
+            self.say(channel, message)
+
+    def setTopic(self, channel, topic, say=False):
+        """
+        Set a channel's topic.
+        """
+        self.publisher.topic(channel, topic)
+        if say:
+            msg = "Channel topic change: %s" % topic
+            self.say(channel, msg)
+
+    def setMultiTopics(self, data, say=False):
+        """
+        Set the topic for multiple channels at once. The 'data' parameter is a
+        dict with the keys being the channel names and the values being the
+        desired channel topic. If the 'say' parameter is defined, the new topic
+        will also be sent as a public message on the channel.
+        """
+        for channel, topic in data.items():
+            self.setTopic(channel, topic, say)
+
+    def setAllTopics(self, topic, say=False):
+        """
+        Set the topic for all defined channels at once.  If the 'say' parameter
+        is defined, the new topic will also be sent as a public message on the
+        channel.
+        """
+        for channel in self.getLoggedChannels():
+            self.setTopic(channel, topic, say)
--- prakasha/app/shell.py
+++ prakasha/app/shell.py
@@ -1,129 +1,20 @@
-import base64
-from glob import glob
 import os
 from pprint import pprint
 import sys
 
 from zope import interface
 
-from twisted.cred import checkers, portal
-from twisted.conch import interfaces, manhole, manhole_ssh, unix
-from twisted.conch.checkers import SSHPublicKeyDatabase
+from twisted.conch import interfaces, manhole_ssh, unix
 from twisted.conch.ssh import filetransfer, session
-from twisted.conch.ssh.channel import SSHChannel
-from twisted.conch.ssh.factory import SSHFactory
-from twisted.conch.ssh.keys import Key
-from twisted.python import components, failure, log
+from twisted.python import components
 
 from dreamssh.app.shell import base as baseshell, pythonshell
 from dreamssh.sdk import registry
 
-from prakasha import exceptions
+from prakasha.app import api
 
 
 config = registry.getConfig()
-
-
-class CommandAPI(pythonshell.CommandAPI):
-
-    def __init__(self, loggerFactory, publisher):
-        super(CommandAPI, self).__init__()
-        self.loggerFactory = loggerFactory
-        self.publisher = publisher
-        self.namespace = None
-
-    def getLoggedChannels(self):
-        """
-        Get the channels that are being logged.
-        """
-        return self.loggerFactory.channels
-
-    def getJoinedChannels(self):
-        """
-        Get the channels to which this client has actually joined.
-        """
-        return self.publisher.userData.keys()
-
-    def getUsers(self, channel):
-        """
-        For a given channel, get the users that have joined it.
-        """
-        return self.publisher.getUsers(channel)
-
-    def ls(self):
-        """
-        List the objects in the current namespace, in alphabetical order.
-        """
-        keys = sorted(self.namespace.keys())
-        pprint(keys)
-
-    def banner(self):
-        """
-        Display the login banner and associated help or info.
-        """
-        print config.ssh.banner
-
-    def joinAll(self):
-        """
-        Join all the channels to which this client is configured to have
-        access.
-        """
-        for channel in self.getLoggedChannels():
-            self.publisher.join(channel)
-
-    def say(self, channel, message):
-        """
-        A convenience wrapper for the IRC client method of the same name.
-        """
-        self.publisher.send(channel, message)
-
-    def sayMulti(self, data, broadcastMessage=""):
-        """
-        Send a public message to multipl channels. The 'data' parameter is a
-        dict with the keys being the channel names and the values being the
-        message to say on that channel. If a 'broadcastMessage' is supplied,
-        the values in the dict are ignored, and all channels will used the
-        broadcast message.
-        """
-        for channel, message in data.items():
-            if broadcastMessage:
-                message = broadcastMessage
-            self.say(channel, message)
-
-    def sayAll(self, message):
-        """
-        Send a public message to all logged channels.
-        """
-        for channel in self.getLoggedChannels():
-            self.say(channel, message)
-
-    def setTopic(self, channel, topic, say=False):
-        """
-        Set a channel's topic.
-        """
-        self.publisher.topic(channel, topic)
-        if say:
-            msg = "Channel topic change: %s" % topic
-            self.say(channel, msg)
-
-    def setMultiTopics(self, data, say=False):
-        """
-        Set the topic for multiple channels at once. The 'data' parameter is a
-        dict with the keys being the channel names and the values being the
-        desired channel topic. If the 'say' parameter is defined, the new topic
-        will also be sent as a public message on the channel.
-        """
-        for channel, topic in data.items():
-            self.setTopic(channel, topic, say)
-
-    def setAllTopics(self, topic, say=False):
-        """
-        Set the topic for all defined channels at once.  If the 'say' parameter
-        is defined, the new topic will also be sent as a public message on the
-        channel.
-        """
-        for channel in self.getLoggedChannels():
-            self.setTopic(channel, topic, say)
 
 
 class ShellInterpreter(pythonshell.PythonInterpreter):
@@ -175,7 +66,7 @@ class ShellTerminalRealm(pythonshell.PythonTerminalRealm):
     def __init__(self, namespace, apiClass):
         baseshell.ExecutingTerminalRealm.__init__(self, namespace)
         if not apiClass:
-            apiClass = CommandAPI
+            apiClass = api.CommandAPI
 
         def getManhole(serverProtocol):
             logService = namespace["services"].getServiceNamed(
